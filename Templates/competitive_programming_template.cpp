a137876bfa1bb3321188c8dff815f37ccc358016
Amit Kumar <offamitkumar@gmail.com>
2020-06-01 13:56:56 +0530

    added competitive programming templates

---
+++ Templates/competitive_programming_template.cpp
@@ -0,0 +1,45 @@
+//  Author : Amit Kumar 
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+//  #include <iostream>
+//  #include <string>
+//  #include <utility>
+//  #include <queue>
+//  #include <stack>
+//  #include <cmath>
+//  #include <map>
+//  #include <unordered_map>
+//  #include <deque>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <climits>
+//  #include <sstream>
+//  #include <iomanip>
+//  #include <cassert>
+//  #include <bitset>
+//  #include <numeric>
+//  #include <vector>
+//  #include <array>
+//  #include <algorithm>
+#include <cstdio>
+using namespace std;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+
+    return 0;
+}
