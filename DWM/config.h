f30fd142ba70b89f29823133fa22598a191e81b8
Amit Kumar <offamitkumar@gmail.com>
2020-06-04 00:17:08 +0530

    updated st at default terminal

--- DWM/config.h
+++ DWM/config.h
@@ -33,7 +33,8 @@ static const Rule rules[] = {
 	 */
 	/* class            instance     title    tags mask        isfloating    monitor */
 	{ "Brave-browser",    NULL,       NULL,       1<<0,                0,           -1 },
-	{ "Alacritty",      NULL,       NULL,       1<<1,             0,           -1 },
+	{ "Alacritty",        NULL,       NULL,       1<<1,             0,           -1 },
+	{ "st-256color",      NULL,       NULL,       1<<1,             0,           -1 },
 	{ "mpv",              NULL,       NULL,       1<<2,             0,           -1 },
 	{ "zoom",             NULL,       NULL,       1<<8,             0,           -1 },
 };
@@ -64,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 
 
