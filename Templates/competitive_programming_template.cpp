f2a55183c66b6120861fc490e779b0309b04b148
Amit Kumar <offamitkumar@gmail.com>
2020-07-08 15:38:09 +0530

    reduced line

--- Templates/competitive_programming_template.cpp
+++ Templates/competitive_programming_template.cpp
@@ -3,12 +3,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 using namespace std::chrono;
+#define int int64_t
 
-int main(void){
+signed main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    ios::sync_with_stdio(false); // FLUSH THE STREAM IF USING puts / printf / scanf/
+    cin.tie(nullptr);            // DISABLE FOR INTERACTIVE PROBLEMS
+    cout.tie(nullptr);           //
 #ifdef HELL_JUDGE
     auto INITIAL_TIME = high_resolution_clock::now();
 #endif 
-
 
 #ifdef HELL_JUDGE
     auto FINAL_TIME = high_resolution_clock::now();
