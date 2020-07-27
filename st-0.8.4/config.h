9dee5d6f9b8503e4170c00aa23f5f18715497e23
Amit Kumar <offamitkumar@gmail.com>
2020-07-27 23:06:32 +0530

    changed fonts

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monaco:pixelsize=21:antialias=true:autohint=true";
+static char *font = "Menlo:pixelsize=21:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
