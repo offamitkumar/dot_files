4d7d4c2b5edd0a498fa69e2f1b07e96ccf6967c1
Amit Kumar <offamitkumar@gmail.com>
2020-07-30 16:11:52 +0530

    updated

--- Templates/def.cpp
+++ Templates/def.cpp
@@ -1,9 +1,11 @@
-//  Author : Amit Kumar 
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+#endif
 
     return 0;
 }
