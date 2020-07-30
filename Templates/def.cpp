e8f7fa757b4d37346ae2803d7d6ec4bad2a7e288
Amit Kumar <offamitkumar@gmail.com>
2020-07-30 16:25:01 +0530

    simple cp template

--- Templates/def.cpp
+++ Templates/def.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int int64_t
 
-int main(void){
+signed main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
 #ifdef HELL_JUDGE
     freopen("input","r",stdin);
     freopen("output","w",stdout);
