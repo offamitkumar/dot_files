84c86f5b1d4a687ee61fa9cdd0f144ad53088ae6
Amit Kumar <offamitkumar@gmail.com>
2020-09-27 15:57:13 +0530

    problem setup

---
+++ problem_setup.sh
@@ -0,0 +1,13 @@
+if [ $# -eq 0 ] 
+then 
+    echo "ERROR : no problem name provided" 
+    exit 
+    fi
+mkdir $1 
+cd $1
+cp ~/Templates/competitive_programming_template.cpp $1.cpp
+cp ~/GitHub/dot_files/Testing_Tools/b.sh .  
+cp ~/GitHub/dot_files/Testing_Tools/gen.cpp .  
+ln -s $1.cpp sol.cpp 
+touch input 
+touch output
