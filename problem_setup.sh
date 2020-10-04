bf5fc3152b1ec451f90ea2e25a16f8ce3207baf8
Amit Kumar <offamitkumar@gmail.com>
2020-10-04 23:41:51 +0530

    04 Oct 2020

--- problem_setup.sh
+++ problem_setup.sh
@@ -1,15 +1,18 @@
 cpy() {
     if [ $# -eq 0 ] 
     then 
-        echo "ERROR : no problem name provided" 
+        cp ~/Templates/competitive_programming_template.cpp sol.cpp
+        cp ~/GitHub/dot_files/Testing_Tools/b.sh .  
+        cp ~/GitHub/dot_files/Testing_Tools/gen.cpp .  
+        touch input 
+        touch output
         return 
         fi
         mkdir $1 
         cd $1
-        cp ~/Templates/competitive_programming_template.cpp $1.cpp
+        cp ~/Templates/competitive_programming_template.cpp sol.cpp
         cp ~/GitHub/dot_files/Testing_Tools/b.sh .  
         cp ~/GitHub/dot_files/Testing_Tools/gen.cpp .  
-        ln -s $1.cpp sol.cpp 
         touch input 
         touch output
     }
